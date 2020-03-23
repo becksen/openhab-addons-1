7fbd91266ec26033632cee6b3e08449440dc0a2c
becksen <becksen@timmys-imac.fritz.box>
2020-03-23 21:38:45 +0100

    change thing type name / define handler

--- bundles/org.openhab.binding.linktap/src/main/java/org/openhab/binding/linktap/internal/linktapBindingConstants.java
+++ bundles/org.openhab.binding.linktap/src/main/java/org/openhab/binding/linktap/internal/linktapBindingConstants.java
@@ -12,6 +12,10 @@
  */
 package org.openhab.binding.linktap.internal;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.smarthome.core.thing.ThingTypeUID;
 
@@ -27,8 +31,15 @@ public class linktapBindingConstants {
     private static final String BINDING_ID = "linktap";
 
     // List of all Thing Type UIDs
-    public static final ThingTypeUID THING_TYPE_SAMPLE = new ThingTypeUID(BINDING_ID, "sample");
+    // public static final ThingTypeUID THING_TYPE_SAMPLE = new ThingTypeUID(BINDING_ID, "sample");
+    public static final ThingTypeUID THING_TYPE_BRIDGE = new ThingTypeUID(BINDING_ID, "gateway");
+
+    // supported linktap device Taplinker TP-1B
+    public static final ThingTypeUID THING_TYPE_TP1B = new ThingTypeUID(BINDING_ID, "TP1B");
+
+    public static final Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = Stream.of(THING_TYPE_BRIDGE, THING_TYPE_TP1B)
+            .collect(Collectors.toSet());
 
     // List of all Channel ids
-    public static final String CHANNEL_1 = "channel1";
+    // public static final String CHANNEL_1 = "channel1";
 }
